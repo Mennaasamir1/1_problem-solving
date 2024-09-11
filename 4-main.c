100b15ed5afc2da107abd31fbba71231823a9bea
Mennaasamir1 <monyy_20174@outlook.com>
2024-09-11 15:04:46 +0300

    code edited

--- 4-main.c
+++ 4-main.c
@@ -3,11 +3,14 @@
 int main(void)
 {
     int number, answer;
+    char str[100];
 
     printf("Enter a number: ");
     scanf("%d", &number);
-    
+
+    printf("Ask a question: ");
+    scanf("%s", str);
     answer = IsPrimeNum(number);
-    printf("is %d a prime number? %d\n", number, answer);
-    
+    printf("%d\n", answer);
+    /*printf("is %d a prime number? %d\n", number, answer);*/
 }
